76db6af43199e59506eb4d559115706fccd9d8db
ragnarune <ragnarunevps@gmail.com>
2025-05-30 10:41:43 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Install upterm (if not already included in Docker image)
-curl -s https://api.github.com/repos/owenthereal/upterm/releases/latest \
-| grep "browser_download_url.*linux_amd64\.tar\.gz" \
-| cut -d '"' -f 4 \
-| wget -qi - -O upterm.tar.gz
-
-mkdir -p upterm
-tar -xzf upterm.tar.gz -C upterm
-mv upterm/upterm /usr/local/bin/
-
-# Generate SSH keys if not exists
+# Generate SSH keys if not present
 if [ ! -f ~/.ssh/id_rsa ]; then
+    mkdir -p ~/.ssh
     ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
 fi
 
-# Start Upterm session
-upterm host --force-command "bash" --server ssh://uptermd.upterm.dev
+echo "📡 Launching Upterm session..."
+upterm host --force-command "bash" --server ssh://uptermd.upterm.dev &
+
+# Wait a few seconds for session to establish
+sleep 5
+
+# Print SSH/Web URL
+upterm session current
+
+# Keep container alive
+while true; do
+  sleep 10
+done
