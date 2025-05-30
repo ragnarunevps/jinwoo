36f820f8bca28b9eaf757336e224174cc71c3451
ragnarune <ragnarunevps@gmail.com>
2025-05-30 11:05:12 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Generate SSH keys if not already present
+# Fail on error
+set -e
+
+# Setup known_hosts to skip "authenticity" prompt
+mkdir -p ~/.ssh
+ssh-keyscan -t ed25519 uptermd.upterm.dev >> ~/.ssh/known_hosts
+
+# Generate SSH key if not present
 if [ ! -f ~/.ssh/id_rsa ]; then
-  mkdir -p ~/.ssh
   ssh-keygen -t rsa -b 3072 -f ~/.ssh/id_rsa -N ""
 fi
 
-# Start Upterm session
-upterm host \
-  --accept \
-  --server ssh://uptermd.upterm.dev:22 \
-  --force-command "tmux attach -t pair || tmux new -s pair" \
-  -- tmux new -s pair
+# Start the upterm host session
+upterm host --accept -- bash
