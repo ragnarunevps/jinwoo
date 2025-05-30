b9fd8e0df7d223a31c613cd0141a2bf86f97ed19
ragnarune <ragnarunevps@gmail.com>
2025-05-30 10:59:08 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Generate SSH keys if not exists
+# Generate SSH keys if not already present
 if [ ! -f ~/.ssh/id_rsa ]; then
   mkdir -p ~/.ssh
   ssh-keygen -t rsa -b 3072 -f ~/.ssh/id_rsa -N ""
 fi
 
-# Start Upterm with correct flags
-upterm host --server ssh://uptermd.upterm.dev:22 --accept --force-command 'tmux attach -t pair-programming || tmux new -s pair-programming'
+# Start Upterm session
+upterm host \
+  --accept \
+  --server ssh://uptermd.upterm.dev:22 \
+  --force-command "tmux attach -t pair || tmux new -s pair" \
+  -- tmux new -s pair
